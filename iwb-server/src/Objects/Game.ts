868a5169384ddaadc5cf7c69dabdd3783e15bfb5
Lastraum <lastraumonline@gmail.com>
2024-05-02 12:28:53 -0700

    Update Game.ts
    - expanded more parameters for game modules

--- iwb-server/src/Objects/Game.ts
+++ iwb-server/src/Objects/Game.ts
@@ -1,6 +1,7 @@
 import {ArraySchema, Schema, type} from "@colyseus/schema";
 import {ACTIONS, COMPONENT_TYPES} from "../utils/types";
 import { Vector3 } from "./Components";
+import { Player } from "./Player";
 
 export class AccessModule extends Schema {
     @type("string") id:string
@@ -35,7 +36,10 @@ export class WaveModule extends Schema {
 export class TimerModule extends Schema {
     @type("string") id:string
     @type("string") type:string
-    @type("number") time:number
+    @type("number") direction:number
+    @type("number") start:number
+    @type("number") end:number
+    @type("string") sound:string
 }
 
 export class Level extends Schema {
@@ -46,6 +50,9 @@ export class Level extends Schema {
     @type("string") type:string
     @type("number") number:number
     @type("boolean") restricted:boolean
+    @type("boolean") showLevel:boolean
+    @type(TimerModule) timer: ArraySchema<TimerModule>
+    @type(TimerModule) countdown: ArraySchema<TimerModule>
 }
 
 export class Game extends Schema {
@@ -55,13 +62,19 @@ export class Game extends Schema {
     @type("string") im:string
     @type("number") type:number
     @type("boolean") editing:boolean
-    @type("string") editLvl:string
+    @type("number") editLvl:number
+    @type("number") startLives:number
+    @type("number") startHealth:number
+    @type("number") startScore:number
+    @type("number") startLevel:number
+    @type("boolean") uiBorder:string
     @type(Vector3) p: Vector3 = new Vector3()
-
-    @type(TimerModule) timer: ArraySchema<TimerModule>
     @type([WaveModule]) waves: ArraySchema<WaveModule>
     @type([Level]) levels: ArraySchema<Level>
     @type([EnemyConfig]) enemies: ArraySchema<EnemyConfig>
+    @type([Player]) players: ArraySchema<Player> // might need something for this for multiplayer to keep track of player health etc
+
+    //win conditions?
 
     //restrict access to # of players in 24 hours?
     //other restrictions?
